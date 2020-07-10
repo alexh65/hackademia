871e920b9021629b66051ac3419d7c41e4cdb254
Alex Hoang <nhuhoang1012@gmail.com>
2020-07-09 21:42:52 -0700

    Return the json data to js file and add svg images

--- script.js
+++ script.js
@@ -1,5 +1,103 @@
 // the json file (will load later)
-var signs
+var signs = {
+  "capricorn":  {
+    "element" : "earth: practical and grounded",
+    "qualities" : "cardinal: take action and start initiatives",
+    "ruler" : "Saturn",
+    "representation" : "The Goat",
+    "perfect match" : "Virgo",
+    "imperfect match" : "Gemini"
+  },
+  "aquarius":  {
+    "element" : "air: intellectual and curious",
+    "qualities" : "fixed: steady, consistent forces that maintain movement",
+    "ruler" : "Uranus",
+    "representation" : "The Water Bearer",
+    "perfect match" : "Sagittarius",
+    "imperfect match" : "Cancer"
+  },
+  "pisces":  {
+    "element" : "water: intuitive and emotional",
+    "qualities" : "mutable: well-suited to change and transformation",
+    "ruler" : "Neptune",
+    "representation" : "The Fish",
+    "perfect match" : "Scorpio",
+    "imperfect match" : "Virgo"
+  },
+  "aries":  {
+    "element" : "fire: passionate and exuberant",
+    "qualities" : "cardinal: take action and start initiatives",
+    "ruler" : "Mars",
+    "representation" : "The Ram",
+    "perfect match" : "Libra",
+    "imperfect match" : "Taurus"
+  },
+  "taurus":  {
+    "element" : "earth: practical and grounded",
+    "qualities" : "fixed: steady, consistent forces that maintain movement",
+    "ruler" : "Venus",
+    "representation" : "The Bull",
+    "perfect match" : "Scorpio",
+    "imperfect match" : "Sagittarius"
+  },
+  "gemini":  {
+    "element" : "air: intellectual and curious",
+    "qualities" : "mutable: well-suited to change and transformation",
+    "ruler" : "Mercury",
+    "representation" : "The Twins",
+    "perfect match" : "Sagittarius",
+    "imperfect match" : "Capricorn"
+  },
+  "cancer":  {
+    "element" : "water: intuitive and emotional",
+    "qualities" : "cardinal: take action and start initiatives",
+    "ruler" : "The Moon",
+    "representation" : "The Crab",
+    "perfect match" : "Taurus",
+    "imperfect match" : "Aquarius"
+  },
+  "leo":  {
+    "element" : "fire: passionate and exuberant",
+    "qualities" : "fixed: steady, consistent forces that maintain movement",
+    "ruler" : "The Sun",
+    "representation" : "The Lion",
+    "perfect match" : "Sagittarius",
+    "imperfect match" : "Scorpio"
+  },
+  "virgo":  {
+    "element" : "earth: practical and grounded",
+    "qualities" : "mutable: well-suited to change and transformation",
+    "ruler" : "Mercury",
+    "representation" : "The Virgin",
+    "perfect match" : "Scorpio",
+    "imperfect match" : "Sagittarius"
+  },
+  "libra":  {
+    "element" : "air: intellectual and curious",
+    "qualities" : "cardinal: take action and start initiatives",
+    "ruler" : "Venus",
+    "representation" : "The Scales",
+    "perfect match" : "Libra",
+    "imperfect match" : "Virgo"
+  },
+  "scorpio":  {
+    "element" : "water: intuitive and emotional",
+    "qualities" : "fixed: steady, consistent forces that maintain movement",
+    "ruler" : "Pluto",
+    "representation" : "The Scorpion",
+    "perfect match" : "Pisces",
+    "imperfect match" : "Aries"
+  },
+  "sagittarius":  {
+    "element" : "fire: passionate and exuberant",
+    "qualities" : "mutable: well-suited to change and transformation",
+    "ruler" : "Jupiter",
+    "representation" : "The Archer",
+    "perfect match" : "Aquarius",
+    "imperfect match" : "Taurus"
+  }
+}
+
 function load(){
     var monthOptions = "";
     var dayOptions = "";
@@ -14,16 +112,15 @@ function load(){
     document.getElementById('months').innerHTML = monthOptions;
     document.getElementById('days').innerHTML = dayOptions;
     
-    loadJSON('resource/signs.json')
     const theForm = document.getElementById('theForm');
     theForm.addEventListener("submit", (e) => {
         e.preventDefault()
         const sign = calculate()
         document.getElementById('result').style.display = 'inline'
         document.getElementById('result_title').innerHTML = 'Your sign is ' + sign
-
+			
         var img = document.createElement("img");
-        let imgsrc = "img/" + sign + ".jpg";
+        let imgsrc = "img/" + sign + ".svg";
         img.src = imgsrc;
         let src = document.getElementById("constellation");
         console.log(src);
@@ -125,6 +222,7 @@ function calculate(){
 
 function showInformation(sign) {
     lowercase_sign = sign.toLowerCase()
+		console.log(signs[lowercase_sign])
     document.getElementById('Element').innerHTML = 'Element: ' + signs[lowercase_sign].element
     document.getElementById('Qualities').innerHTML = 'Qualities: ' + signs[lowercase_sign].qualities
     document.getElementById('Ruler').innerHTML = 'Ruler: ' + signs[lowercase_sign].ruler
@@ -134,8 +232,3 @@ function showInformation(sign) {
     
   }
 
-async function loadJSON(path) {
-    // using Promise
-    let response = await fetch(path);
-    signs = await response.json();
-}

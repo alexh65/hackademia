dfaaf513eb205e99e6ed92ec07fe108152975c12
Alex Hoang <nhuhoang1012@gmail.com>
2020-07-10 23:27:24 -0700

    Add some css to it

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function load(){
     }
     document.getElementById('months').innerHTML = monthOptions;
     document.getElementById('days').innerHTML = dayOptions;
-    
-    const theForm = document.getElementById('theForm');
+	
+    const theForm = document.getElementById('theForm')
     theForm.addEventListener("submit", (e) => {
         e.preventDefault()
         const sign = calculate()
@@ -35,6 +35,7 @@ function load(){
         }
 
         showInformation(sign);
+				
     })
 }
 
